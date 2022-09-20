07fd8a69e8e41042deab6d4a03838aecd1ed14c4
_Dewelg_ <_(dewel713@gmail.com_)>
2022-09-19 20:12:58 -0400

    part 2 done

--- app.js
+++ app.js
@@ -6,19 +6,54 @@ console.log(
 // Exercise 1
 let a = 20;
 let b = 4
-let add = a + b;
+//let add = a + b;
 let minus = a - b;
 let dividing = a / b;
 console.log();
 
-// YOUR CODE HERE
+
 
 // Exercise 2
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+//Answers to questions
+// - 1111
+// - 11eleven
+// - true
+// - Frodo11
+// - true11
+// - FrodoBaggins
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+//this is a faster way 
+function answers(){
+  let sum = num + str;
+  console.log(sum);
+
+  let sum2 = num + str2;
+  console.log(sum2);
+
+  let sum3 = num + isPresent;
+  console.log(sum3);
+
+  let sum4 = firstName + num;
+  console.log(sum4);
+
+  let sum5 =  isPresent + str;
+  console.log(sum5);
+
+  let sum6 = firstName + lastName;
+  console.log(sum6);
+
+}
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
